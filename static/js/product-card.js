1c1b45ca43b3f35de4dfbd8d3301394e9e0f4b43
Harsh Kapadia <harshgkapadia@gmail.com>
2020-10-14 11:28:37 +0530

    :sparkles: feat: Add `product-card` web component

---
+++ static/js/product-card.js
@@ -0,0 +1,68 @@
+const template = document.createElement("template");
+template.innerHTML = `
+	<style>
+		.product-card
+		{
+			display: grid;
+			place-content: center center;
+			width: 17em;
+			border: 1px solid #e0e0e0;
+			border-radius: 2px;
+		}
+
+		a
+		{
+			text-decoration: none;
+			color: black;
+		}
+
+		img
+		{
+			width: 15em;
+			padding: 1em;
+		}
+
+		.product-info
+		{
+			padding: 0 1em 1em 1em;
+		}
+
+		.product-title
+		{
+			font-size: 18px;
+		}
+
+		.cost
+		{
+			font-size: 25px;
+			padding-top: 0.5em;
+		}
+	</style>
+
+	<div class="product-card">
+		<a href="#"><img /></a>
+
+		<div class="product-info">
+			<a href="#"><div class="product-title"></div></a>
+
+			<div class="cost">Rs. <span class="product-cost"></span></div>
+		</div>
+	</div>
+`;
+
+class ProductCard extends HTMLElement
+{
+	constructor()
+	{
+		super();
+
+		this.attachShadow({ mode: "open" });
+		this.shadowRoot.appendChild(template.content.cloneNode(true));
+		
+		this.shadowRoot.querySelector("img").src = this.getAttribute("img");
+		this.shadowRoot.querySelector(".product-title").innerText = this.getAttribute("name");
+		this.shadowRoot.querySelector(".product-cost").innerText = this.getAttribute("price");
+	}
+}
+
+window.customElements.define("product-card", ProductCard);
