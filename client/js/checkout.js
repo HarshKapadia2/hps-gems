e4674d86ed9d8b38c877b3b1e09c1f9df5b12a3b
Harsh Kapadia <harshgkapadia@gmail.com>
2020-11-27 21:32:03 +0530

    :sparkles: feat: Add checkout page data display functionality

---
+++ client/js/checkout.js
@@ -0,0 +1,199 @@
+const error_div = document.querySelector("#errors");
+const address = document.querySelector("#address");
+const order_date = document.querySelector("#order-date");
+const items = document.querySelector("#items");
+const item_img = document.querySelector(".item-img");
+const item_name = document.querySelector(".item-name");
+const item_rate = document.querySelector(".item-rate");
+const total_items_price = document.querySelector("#total-items-price");
+const shipping_price = document.querySelector("#shipping-price");
+const tax = document.querySelector("#tax");
+const final_price = document.querySelector("#final-price");
+const order_btn = document.querySelector("button");
+
+let errors = [];
+let total_price = 0;
+
+const token = localStorage.getItem("hpsgemstoken");
+
+window.addEventListener
+(
+	"load",
+	async () =>
+	{
+		if(await auth())
+			getCheckoutDetails();
+	}
+);
+
+
+async function auth()
+{
+	if(!token)
+	{
+		createNavLink("Log In/Sign Up", "./signup.html");
+
+		order_btn.disabled = true;
+
+		errors.push("Log in to view checkout.");
+		displayErrors();
+
+		return false;
+	}
+
+	let fetch_result = false;
+
+	fetch_result = await fetch
+	(
+		"/hps-gems/server/api/auth.php",
+		{
+			headers: {
+				"Accept": "application/json",
+				"Authentication": `Bearer ${token}`
+			}
+		}
+	).then
+	(
+		(res) =>
+		{
+			if(res.ok)
+				return res.json();
+			else
+			{
+				errors.push("Failure in fetching data.");
+				displayErrors();
+
+				throw new Error("Failure in fetching data.");
+			}
+		}
+	).then
+	(
+		(res_data) =>
+		{
+			if(res_data.code === 200)
+			{
+				createNavLink("Cart", "./cart.html");
+				createNavLink(`${res_data.data.first_name} ${res_data.data.last_name}`, "./profile.html");
+				
+				return true;
+			}
+			else
+			{
+				createNavLink("Log In/Sign Up", "./signup.html");
+
+				errors.push("Log in to view checkout.");
+				displayErrors();
+
+				throw new Error(`${res_data.code} ${res_data.status}: ${res_data.errors}`);
+			}
+		}
+	).catch((err) => { console.error(err); return false; });
+
+	return fetch_result;
+}
+
+function getCheckoutDetails()
+{
+	fetch
+	(
+		"/hps-gems/server/api/checkout.php",
+		{
+			headers: {
+				"Accept": "application/json",
+				"Authentication": `Bearer ${token}`
+			}
+		}
+	).then
+	(
+		(res) =>
+		{
+			if(res.ok)
+				return res.json();
+			else
+			{
+				errors.push("Failure in fetching data.");
+				displayErrors();
+
+				throw new Error("Failure in fetching data.");
+			}
+		}
+	).then
+	(
+		(res_data) =>
+		{
+			if(res_data.code === 200)
+			{
+				address.innerText = `Address: ${res_data.data.address}`;
+				order_date.innerText = `Order date: ${Date.now()}`;
+				shipping_price.innerText = "Rs. 100";
+
+				for(let i = 0; i < res_data.data.item_count; i++)
+				{
+					let item_data = res_data.data.items[i];
+					createItem(item_data.pic_url, item_data.name, item_data.qty, item_data.price);
+				}
+
+				total_items_price.innerText = `Rs. ${total_price}`;
+				tax.innerText = `Rs. ${0.02 * total_price}`;
+				final_price.innerText = `Rs. ${100 + (0.02 * total_price) + total_price}`;
+			}
+			else
+			{
+				createNavLink("Log In/Sign Up", "./signup.html");
+
+				errors = res_data.data.errors;
+				displayErrors();
+
+				throw new Error(`${res_data.code} ${res_data.status}: ${res_data.errors}`);
+			}
+		}
+	).catch((err) => console.error(err));
+}
+
+function createItem(pic_url, name, qty, price)
+{
+	let item_div = document.createElement("div");
+	let img_div = document.createElement("div");
+	let img_tag = document.createElement("img");
+	let name_div = document.createElement("div");
+	let rate_div = document.createElement("div");
+
+	item_div.classList.add("item");
+	img_div.classList.add("item-img");
+	img_tag.src = pic_url;
+	name_div.classList.add("item-name");
+	name_div.innerText = name;
+	rate_div.classList.add("item-rate");
+	rate_div.innerText = `${qty} x ${price} = Rs. ${parseInt(qty) * parseFloat(price)}`;
+
+	img_div.appendChild(img_tag);
+	item_div.appendChild(img_div);
+	item_div.appendChild(name_div);
+	item_div.appendChild(rate_div);
+	items.appendChild(item_div);
+
+	total_price += parseInt(qty) * parseFloat(price);
+}
+
+function createNavLink(text, url)
+{
+	let a = document.createElement("a");
+	let li = document.createElement("li");
+
+	li.innerText = text;
+	a.href = url;
+	
+	a.appendChild(li);
+	links.appendChild(a);
+}
+
+function displayErrors()
+{
+	for(let i = 0; i < errors.length; i++){
+		let div = document.createElement("div");
+		div.innerText = errors[i];
+		error_div.appendChild(div);
+	}
+
+	errors = [];
+}
---
+++ server/api/checkout.php
@@ -0,0 +1,58 @@
+<?php
+	header("Content-Type: application/json");
+	header("Accept: application/json");
+	
+	include_once("../config/Database.php");
+	include_once("../models/Auth.php");
+	include_once("../models/User.php");
+	include_once("../models/OrderDetail.php");
+
+	$database = new Database();
+	$db = $database->connect();
+
+	$auth = new Auth($db);
+	$user = new User($db);
+	$order_detail = new OrderDetail($db);
+
+	$errors = [];
+
+	$auth_header_val = isset($_SERVER["HTTP_AUTHENTICATION"]) ? $_SERVER["HTTP_AUTHENTICATION"]: "";
+
+	if($auth_header_val != "")
+	{
+		$token = substr($auth_header_val, 7);
+	
+		$auth->token = $token;
+		$auth_result = $auth->authenticate();
+	
+		if($auth_result["status"] === "success")
+		{
+			$user->id = $auth_result["data"]["user_id"];
+			$user_result = $user->getUser();
+
+			if($user_result["status"] === "success")
+			{
+				$order_detail->user_id = $auth_result["data"]["user_id"];
+				$order_result = $order_detail->getOrder();
+	
+				if($order_result["status"] === "success")
+				{
+					$final_data = array(
+						"address" => $user_result["data"]["address"],
+						"item_count" => $order_result["data"]["item_count"],
+						"items" => $order_result["data"]["items"]
+					);
+					echo json_encode(array("status" => "OK", "code" => 200, "data" => $final_data, "errors" => array()));
+				}
+				else
+					echo json_encode(array("status" => "INTERNAL SERVER ERROR", "code" => 500, "data" => array(), "errors" => array("Database error")));
+			}
+			else
+				echo json_encode(array("status" => "INTERNAL SERVER ERROR", "code" => 500, "data" => array(), "errors" => array("Database error")));
+		}
+		else
+			echo json_encode(array("status" => "UNAUTHORIZED", "code" => 401, "data" => array(), "errors" => array("Please log in to add product to cart.")));
+	}
+	else
+		echo json_encode(array("status" => "NOT ACCEPTABLE", "code" => 406, "data" => array(), "errors" => array("Error in receiving data.")));
+?>
